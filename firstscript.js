b93c055325583172c90e6d6cef2baf7477e83c88
Durgesh Mishra <107487348+gitgetvantage@users.noreply.github.com>
2022-06-21 13:07:06 +0530

    Js Modified

--- firstscript.js
+++ firstscript.js
@@ -1,2 +1,19 @@
-let exa = " " + " ";
-console.log(exa);
+// let exa = " " + " ";
+// console.log(exa);
+// Number.NaN === NaN
+// let x = 10;
+// let y = 10;
+// console.log("Hello here we are check assingnment opertor",  x == y);
+// console.log(`Is x and y are equal : ${x==y}`);
+// Increment and Decrement operator
+
+// let num = 10;
+// let newNum = num++ + 5;
+// // sumNum = newNum;
+// console.log(num);
+// console.log(newNum);
+let num = 10;
+let newNum = ++num + 5;
+// sumNum = newNum;
+console.log(num);
+console.log(newNum);
